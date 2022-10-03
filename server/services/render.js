6a09c8f9a39930c949245d26b9edfce9c538b880
Pirin <chuckdanorris123@gmail.com>
2022-10-03 13:27:10 -0400

    fixed typos

--- server/services/render.js
+++ server/services/render.js
@@ -1,26 +1,29 @@
 const axios = require('axios');
 
+
 exports.homeRoutes = (req, res) => {
-    // make a get request to /api/ users
+    // Make a get request to /api/users
     axios.get('http://localhost:3000/api/users')
-      .then(function(response){
-        res.render('index', {users: response.data});
-      })
-      .catch(err => {
-        res.send(err);
-      })
+        .then(function(response){
+            res.render('index', { users : response.data });
+        })
+        .catch(err =>{
+            res.send(err);
+        })
+
+    
 }
 
-exports.add_user = (req,res) => {
+exports.add_user = (req, res) =>{
     res.render('add_user');
 }
 
-exports.update_user = (req,res) => {
-    axios.get('http://localhost:3000/api/users', {params: {id:req.query.id}})
-    .then(function(userdata){
-      res.render("update_user", {user:userdata.data})
-    })
-    .catch(err =>{
-      res.send(err);
-    })
+exports.update_user = (req, res) =>{
+    axios.get('http://localhost:3000/api/users', { params : { id : req.query.id }})
+        .then(function(userdata){
+            res.render("update_user", { user : userdata.data})
+        })
+        .catch(err =>{
+            res.send(err);
+        })
 }
